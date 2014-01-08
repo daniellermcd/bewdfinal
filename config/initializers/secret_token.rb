88e31d37fbeb4c29ef9242da455e4a340cca1f7c
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-07 20:46:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photobooth::Application.config.secret_key_base = 'b54a61f3c239de82d49ca3b1d54c6e4446a5faf0c518dbe21bfeb4d0dae89cf79f19d3dcfcb56cf1c7201dbf6e27ee95ccbf1f778598082acc18664c1346829e'
