40a4c36edae6229dbcc0acf86ce5a523e8c73cd4
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-08 23:58:49 -0500

    user can create new album, must log in first

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,20 @@
 class AlbumsController < ApplicationController
+  def show
+    @album = Album.find params[:id]
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    safe_album_params = params.require(:album).permit(:title)
+    @album = current_user.albums.new safe_album_params
+
+    if @album.save
+      redirect_to @album
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @albums = current_user.albums
+    end
   end
 end
