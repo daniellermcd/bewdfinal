137b243f02882aba74c01431dc2917200455491b
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-09 22:31:38 -0500

    refining routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Photobooth::Application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   root "home#index"
-  resources :photos
-  resources :albums
+  resources :photos, only: :create
+  resources :albums, only: [:new, :create, :show]
 end
