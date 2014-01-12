31c539d762b23bfe0764f0680ba00df6ffc841e5
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-12 05:39:42 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Photobooth::Application.routes.draw do
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   root "home#index"
   resources :photos, only: :create
   resources :albums, only: [:new, :create, :show]
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 end
