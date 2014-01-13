52b9f53f9c7ce978a98abccc7d62e5f08586f029
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-12 21:09:31 -0500

    redirect to photobooth after photo is saved

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,13 +2,14 @@ class PhotosController < ApplicationController
   def create
     album_id = params[:album_id]
     album = Album.find album_id
+    photobooth = "/albums/#{album_id}/photobooth"
     imageData = params[:image_data_url]['data:image/png;base64,'.length .. -1]
 
     photo = convert_to_png(imageData)
 
     photo_url = Photo.get_s3_url(album_id)
     album.photos.create url: photo_url
-    redirect_to album
+    redirect_to photobooth
   end
 
   private
