5457d5b8b9a9cb022776f9be223279e47af4388a
Danielle McDonnough <daniellermcd@gmail.com>
2014-01-13 04:22:04 -0500

    add ability to edit album, updating routes, refactoring

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,8 @@
 class AlbumsController < ApplicationController
   force_ssl unless Rails.env.development?
 
+  before_action :set_album, only: [:photobooth, :show, :edit, :update]
+  before_action :set_username, only: [:photobooth, :show]
   before_action :authenticate_user!
   before_filter :validate_user, only: [:show, :photobooth]
 
@@ -9,13 +11,9 @@ class AlbumsController < ApplicationController
   end
 
   def photobooth
-    @album = album
-    @username = username
   end
 
   def show
-    @album = album
-    @username = username
     @album_photos = @album.photos
   end
 
@@ -24,7 +22,6 @@ class AlbumsController < ApplicationController
   end
 
   def create
-    safe_album_params = params.require(:album).permit(:title)
     @album = current_user.albums.new safe_album_params
 
     if @album.save
@@ -34,17 +31,29 @@ class AlbumsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @album.update safe_album_params
+    redirect_to @album
+  end
+
   private
-  def username
+  def set_username
     @album.user.email[/[^@]+/]
   end
 
-  def album
-    Album.find params[:id]
+  def set_album
+    @album = Album.find params[:id]
   end
 
   def validate_user
     album_owner = Album.find(params[:id]).user
     redirect_to root_path unless current_user and current_user.id == album_owner.id
+  end
+
+  def safe_album_params
+    safe_album_params = params.require(:album).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Photobooth::Application.routes.draw do
   root "home#index"
   resources :photos, only: :create
-  resources :albums, only: [:new, :create, :show]
+  resources :albums
   get '/albums', to: 'albums#index'
   get '/albums/:id/photobooth', to: 'albums#photobooth'
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
